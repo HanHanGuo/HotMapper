1905c4ee3d89ad013e5334c47e10afa0050316c2
Administrator <Administrator@PC-20181102KZHE>
2019-02-19 16:16:40 +0800

    增加基础service的表和实体转换方法

--- src/main/java/com/xianguo/hotmapper/service/impl/BaseServiceImpl.java
+++ src/main/java/com/xianguo/hotmapper/service/impl/BaseServiceImpl.java
@@ -1,9 +1,12 @@
 package com.xianguo.hotmapper.service.impl;
 
 import java.lang.reflect.ParameterizedType;
+import java.util.List;
+import java.util.Map;
 
 import com.xianguo.hotmapper.bean.Table;
 import com.xianguo.hotmapper.container.Container;
+import com.xianguo.hotmapper.util.PreparedStatementUtil;
 
 /**
  * 基础service实现类，对hotmapper加载信息到hotmapper容器
@@ -20,6 +23,30 @@ public abstract class BaseServiceImpl<T,DAO> {
 	protected BaseServiceImpl() {
     	classes = (Class <T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     	table = Container.load(classes);
+    }
+    
+    /**
+     * 处理表和实体的转换
+     * @author 鲜果
+     * @date 2019年2月19日
+     * @param maps
+     * @return
+     * List<T>
+     */
+    public List<T> MapToBean(List<Map<String, Object>> maps){
+    	return PreparedStatementUtil.convertBeanByList(classes, maps, table);
+    }
+    
+    /**
+     * 处理表和实体的转换
+     * @author 鲜果
+     * @date 2019年2月19日
+     * @param maps
+     * @return
+     * List<T>
+     */
+    public T MapToBean(Map<String, Object> map) {
+    	return PreparedStatementUtil.convertBeanByMap(classes, map, table);
     }
 	
 	/**
